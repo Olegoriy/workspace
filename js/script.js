4dae357a13551f7363a124f6020fa01fd264a0b6
Olegoriy <olegkib1506@gmail.com>
2024-11-02 23:23:20 +0300

    added function getData for get cities from API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
 const citySelect = document.querySelector('#city');
 const cityChoices = new Choices(citySelect, {
-    searchEnabled: false,
     itemSelectText: "",
 });
+
+const getData = async (url, cbSuccess, cbError) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        cbSuccess(data);
+    } catch(err) {
+        cbError;
+    }
+}
+
+getData(API_URL + LOCATION_URL,
+    (data) => {
+        console.log(data);
+    },
+    (err) => {
+        console.log(err);
+    },
+);
