ca80605c7a6257667cad3c1b47c5b0f66fb494fe
Olegoriy <olegkib1506@gmail.com>
2024-11-04 17:54:10 +0300

    added vacancies from server

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const API_URL = "https://workspace-methed.vercel.app/";
 const LOCATION_URL = "api/locations";
+const VACANCY_URL = "api/vacancy";
 
+const cardsList = document.querySelector('.cards__list');
 
 const getData = async (url, cbSuccess, cbError) => {
     try {
@@ -12,10 +14,48 @@ const getData = async (url, cbSuccess, cbError) => {
     }
 }
 
+const createCard = vacancy => `
+    <article class="vacancy" tabindex="0" data-id="${vacancy.id}">
+        <img src="${API_URL}${vacancy.logo}" alt="Логотип компании ${vacancy.company}" class="vacancy__img">
+
+        <p class="vacancy__company">${vacancy.company}</p>
+
+        <h3 class="vacancy__title">${vacancy.title}</h3>
+
+        <ul class="vacancy__fields">
+            <li class="vacancy__field">от ${parseInt(vacancy.salary).toLocaleString()}₽</li>
+            <li class="vacancy__field">${vacancy.format}</li>
+            <li class="vacancy__field">${vacancy.type}</li>
+            <li class="vacancy__field">${vacancy.experience}</li>
+        </ul>
+    </article>
+`;
+
+const createCards = (data) => 
+    data.vacancies.map(vacancy => {
+        const li = document.createElement('li');
+        li.classList.add('cards__item');
+        li.insertAdjacentHTML('beforeend', createCard(vacancy));
+        return li;
+    })
+
+
+const renderVacancy = data => {
+    cardsList.textContent = "";
+    const cards = createCards(data);
+    cardsList.append(...cards);
+}
+
+const renderError = err => {
+    console.warn(err);
+}
+
+
 const init = () => {
+    // select
     const citySelect = document.querySelector('#city');
-const cityChoices = new Choices(citySelect, {
-    itemSelectText: "",
+    const cityChoices = new Choices(citySelect, {
+        itemSelectText: "",
 });
 
     getData(
@@ -34,8 +74,14 @@ const cityChoices = new Choices(citySelect, {
         (err) => {
             console.log(err);
         },
-    );    
-}
+    );  
+    
+    //cards
+    const url = new URL(`${API_URL}${VACANCY_URL}`);
+
+    getData(url, renderVacancy, renderError);
+    
+};
 
 init()
 
